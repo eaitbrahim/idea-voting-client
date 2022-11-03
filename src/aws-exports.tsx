aea4bd4e6d0a1e212a8b2abcf6bd9d2e7413c2fc
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-11-03 07:22:57 -0400

    Create project & add amplify authenticator & sign out functionalities

---
+++ src/aws-exports.tsx
@@ -0,0 +1,8 @@
+const awsConfig = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_gD06emCYs',
+  aws_user_pools_web_client_id: '6c3acg5s4r92khrhnpjruim2qo'
+};
+
+export default awsConfig;
